5665dfdf38cfde385f0f05614d5154baddd174d5
mhmdrz <smrm1381@yahoo.com>
2021-11-05 22:21:09 +0330

    Session 1 completed

---
+++ Session_1/Question_1.py
@@ -0,0 +1,15 @@
+header_footer_stars = 48*'*'
+stars3 = 3*'*'
+stars5 = 5*'*'
+
+name = 'Abbas'
+last_name = 'Bouazar'
+city = 'Ahvaz'
+
+print(header_footer_stars)
+print(header_footer_stars)
+print(stars3 + 19*' ' + name + 18*' ' + stars3)
+print(stars5 + 16*' ' + last_name + 15*' ' + stars5)
+print(stars3 + 19*' ' + city + 18*' ' + stars3)
+print(header_footer_stars)
+print(header_footer_stars)
---
+++ Session_1/Question_4.py
@@ -0,0 +1,21 @@
+n1 = '4F'
+n2 = '6C'
+n3 = '73'
+n4 = '73'
+n5 = '76'
+print(int(n1, 16))
+print(int(n2, 16))
+print(int(n3, 16))
+print(int(n4, 16))
+print(int(n5, 16))
+
+n6 = '1000100'
+n7 = '1110110'
+n8 = '1111001'
+n9 = '1110011'
+n10 = '1101011'
+print(int(n6, 2))
+print(int(n7, 2))
+print(int(n8, 2))
+print(int(n9, 2))
+print(int(n10, 2))
